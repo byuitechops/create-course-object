1934a182134b8274347d711b18f75c066b37cf27
Zachary Williams <wil11063@byui.edu>
2017-11-28 12:24:02 -0700

    Attempting ansi to html

--- Course.js
+++ Course.js
@@ -101,7 +101,7 @@ module.exports = class Course {
             if (this.settings.debug) {
                 console.log(
                     fws(chalk.cyan(moduleName), 15),
-                    fws(convert(chalk.greenBright('SUCCESS')), 8, { align: 'right'})),
+                    fws(convert(chalk.greenBright('SUCCESS')), 8, { align: 'right'}),
                     chalk.white(message)
                 );
             }
