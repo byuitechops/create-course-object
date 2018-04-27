8e33193f4fd5ef3ebd682997b3cb72b8670d6172
Zachary Williams <wil11063@byui.edu>
2018-04-27 09:53:35 -0600

    Add tests (not operable yet)

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,14 +3,26 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
 
-        test.pass('potato');
-        test.pass('tomato');
-        test.fail('avacado');
-
-        test.end();
-    });
+    tap.ok(course);
+    tap.ok(course.settings);
+    tap.ok(course.info);
+    tap.ok(course.content);
+    tap.ok(course.logger);
+    tap.ok(course.log);
+    tap.ok(course.warning);
+    tap.ok(course.error);
+    tap.ok(course.message);
+    tap.ok(course.fatalError);
+    tap.ok(course.console);
+    tap.ok(course.info.originalZipPath);
+    tap.ok(course.info.unzippedPath);
+    tap.ok(course.info.processedPath);
+    tap.ok(course.info.uploadZipPath);
+    tap.ok(course.info.fileName);
+    tap.ok(course.info.canvasOU);
+    tap.ok(course.info.courseName);
+    tap.ok(course.info.courseCode);
 
     callback(null, course);
 };
