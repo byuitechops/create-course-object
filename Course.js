9eda0a4b7a6fd66d2bc90434ed01fdc8df11c346
Zachary Williams <wil11063@byui.edu>
2018-06-15 12:34:52 -0600

    Hopeful coursecode fix

--- Course.js
+++ Course.js
@@ -64,7 +64,9 @@ module.exports = class Course {
         };
 
         this.info.courseName = this.info.fileName.split('.zip')[0];
-        this.info.courseCode = this.info.fileName.split('.zip')[0];
+        this.info.courseCode = (this.info.fileName.split(' ')[0] + ' ' + this.info.fileName.split(' ')[1]).replace(':', '');
+        console.log('COURSE NAME:', this.info.courseName);
+        console.log('COURSE CODE:', this.info.courseCode);
 
         /* Set up the logger */
         this.logger = logger;
